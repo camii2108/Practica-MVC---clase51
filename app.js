f055e0f79aa15c1eba3d8abbf1941c6303978d89
camila2108 <gutierrezcamilamilagros02@gmail.com>
2023-02-09 22:48:46 -0300

    Micro desafío - Paso 4

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-const express = require('express');
-const app = express();
+const express = require('express');/* requerimos en modulo de express */
+const app = express();/* nos retorna un funcion */
+
+app.listen(3000, ()=> console.log('Server listen'))
